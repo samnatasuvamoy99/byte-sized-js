19e740180d43261700d15baec56d20a89dba1d68
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-17 20:02:50 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId= 458648212
+let accountEmail="suvamoysamanta907@gmail.come"
+var accountPassword="854695"
+accountCity="kolkata"
+
+let accountStatus;
+
+//accountId = 2 // not allowed  this bc already const that
+
+console.log(accountId);
+
+accountEmail="suvomoy.come"
+accountPassword="658549896"
+accountcity="delhi"
+/*
+prefer not to use var 
+because of the issue in the block scope and functional scope 
+*/
+ console.table([accountId , accountEmail , accountPassword , accountCity])

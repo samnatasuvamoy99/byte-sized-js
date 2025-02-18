f1885393661fa71e3a4503c7322231c80a899b03
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-18 14:33:09 +0000

    datatype commit

---
+++ 01_basics/03datatype_convertion.js
@@ -0,0 +1,56 @@
+let score= true
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueInnumber=Number(score)
+//console.log(valueInnumber);
+
+//"33"=>33
+//"33abc"=>NAN->not a number
+//true = 1 , false=>0;
+
+let isLoggedIn="true"
+let booleanIsLogged=Boolean(isLoggedIn)
+//console.log(booleanIsLogged)
+
+// 1=>true , 0 => false , "suvamoy"=>true;
+
+
+let someNumber = 343
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log( typeof stringNumber)
+
+//******************operation****************
+let value = 3
+let negValue =-value
+
+//console.log(negValue)
+
+//console.log( 3* 4)
+//console.log(4 - 7)
+//console.log(5 % 6)  // all mathamatic operation will be held on
+
+
+let str1="suvamoy"
+let str2 ="  samanta"
+let str3= str1+str2
+console.log(str3)
+
+//another opertion //!
+
+//console.log("1"+2)
+ //console.log(1+"2")
+ //console.log("2"+3+4)
+ //console.log( 3+4+"5")
+ //console.log((3+ 4) * 5 -(5-2)) // alaways will use parenthasis backet 
+
+ let num2=num4=num5=5
+
+ let gamecounter=230
+ ++gamecounter;
+ console.log(gamecounter)
+
+
+
+

e0f1b8e6a1a532a7af5118e70255129b72733055
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-19 16:32:53 +0000

    String detials .js

---
+++ 01_basics/07String.js
@@ -0,0 +1,38 @@
+const Name ="suvamoy"
+const repoCount =50
+
+//console.log( Name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`); // another types of string decleartion  backtrik;
+
+const gameName = new String('suvamoy_saman') // string decleartion advance
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+
+// how to access all prototype in this previous string ......
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+
+/*console.log(gameName.charAt(2))
+console.log(gameName.indexOf("v"))
+
+//substring methode
+const newString=gameName.substring(0,5)
+console.log(newString)
+
+const anotherString=gameName.slice(-9,7)
+console.log(anotherString)
+*/
+const newstringm="    nabanita     "
+console.log(newstringm);
+console.log(newstringm.trim())
+
+
+const url = "https://suvamoy.com/suvamoy%20samanta"
+console.log(url.replace('%20','_'))
+console.log(url.includes('suvamoy'))
+
+console.log(gameName.split('_'));// any string to split to array
+
+
+

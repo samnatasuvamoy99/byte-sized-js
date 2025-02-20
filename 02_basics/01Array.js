ce4c450332b97bccd6cb04d710a18184becbbefa
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-20 13:35:04 +0000

    array starting ...

---
+++ 02_basics/01Array.js
@@ -0,0 +1,47 @@
+//Array
+/*const maArr = [0, 1 , 3 , 5]
+console.log(maArr[0]); // access the array element 
+
+const myHero=["suvamoy" , "ironman" ,"doremon"]
+ const myArry= new Array(1,3,5,6) // this the another type of array decleartion 
+ console.log(myArry[3]) */
+
+ /*
+ // array another method 
+ const Array =[25,56,89,23,45]
+ Array.push(78)
+ Array.push(456)
+ console.log(Array)
+ Array.pop()
+ console.log(Array)
+ */
+
+ const Array =[25,56,89,23,45]
+ /*
+ Array .unshift(0) 
+ console.log(Array);
+ Array.shift()
+ console.log(Array);
+ 
+ console.log(Array.includes(100))
+ console.log(Array.indexOf(89))
+
+ // convert any array into string
+ const newArr= Array.join()
+ console.log(newArr)
+ */
+
+ console.log("A ", Array); // slice and splice
+
+ const myn1 =Array.slice(1, 3)
+ console.log("B " , Array)
+ console.log(myn1)
+ 
+
+ const myn2 =Array.splice(1 , 3)
+ console.log("c " ,Array)
+ console.log(myn2)
+ 
+ 
+    
+

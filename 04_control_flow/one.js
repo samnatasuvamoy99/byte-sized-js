b4f67f1ec50e1ca489d265dea12ed176edd7465c
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-24 21:27:59 +0000

    control flow basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+//if
+/*
+const isUserloggedIn = true
+const temperature = 41
+
+if (temperature < 39){
+     console.log("less than 50")
+}
+else{
+   console.log("tempature is greater then 50")
+}
+console.log("tempature execute")
+*/
+/*
+const score = 200
+if ( score > 100){
+    let power = "fly"
+  console.log(`user power: ${power}`);
+}
+console.log(`user power:${power}`);
+*/
+
+const balance =1000
+//if (balance > 500) console.log("test");
+/*
+  //nested if else
+   if ( balance < 500){
+    consoe.log("less than")
+   }
+   else if ( balance < 750){
+        console.log(" less then 750")
+   }
+   else{
+    console.log("less than 1200")
+   }
+    */
+
+   /*
+   const isUserloggedIn = true;
+   const debitcard=true;
+   if (isUserloggedIn && debitcard){
+    console.log("Allow the buy course");
+    
+   }
+    */
+
+   const loggedInfromGoogle =false
+   const loggedInfromEmail = true;
+   if ( loggedInfromEmail || loggedInfromGoogle){
+    console.log("your loggedin succesfull")
+   }

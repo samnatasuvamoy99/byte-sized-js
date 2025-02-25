139459b2a4812a4e2467c47942a20ee43fe7ef5a
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-02-25 20:27:54 +0000

    while and do while

---
+++ 05_iterations/02loop.js
@@ -0,0 +1,19 @@
+let index=0
+while ( index <= 10) {
+  //console.log(`value of index is ${index}`);
+  index= index + 3;
+}
+
+let myArray =["suvamoy" , "samanta","nabanita" ,"bera"]
+ let i =0;
+while (  i < myArray.length ){
+  // console.log(`my name is / title ${myArray[i]}`)
+   i++
+}
+//do while loop 
+  let score =19
+do{
+    console .log(`score is ${score}`);
+    score++;
+} while( score <= 10);
+

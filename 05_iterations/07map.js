5c21858035dbb89cecf080ba349f65602de5470a
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-01 13:51:34 +0000

    map

--- 05_iterations/07map.js
+++ 05_iterations/07map.js
@@ -7,8 +7,17 @@ const number=[1,2,3,4,5,6,7,8,9]
    return num + 89
 })
     */
+   /*
     const userbooks=[]
 number.forEach( (num) => {
-       userbooks.push(num + 89)
+       userbooks.push(num + 89) // push in map
 });
- console.log(userbooks)
+ console.log(userbooks) */
+
+ const newNUm=number
+ .map((num) => num * 10)  // multiple 10
+ .map((num) => num + 1)  // affter multiple add + 1 ;
+ .filter((num) => num >= 40)
+
+ console.log(newNUm);
+ 

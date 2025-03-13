153cbb65541ce3b4529d763636eb7950648cf339
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-13 05:29:12 +0000

    colorchanges

--- project1_colorchanges/colourchange.js
+++ project1_colorchanges/colourchange.js
@@ -4,3 +4,26 @@ console.log("suvamoy")
  const buttons =document.querySelectorAll('.button');
 const body = document.querySelector("body")
 
+buttons.forEach(function(button){
+  console.log(button);
+  // this  addeven the method that we can listening their footstep ....
+     button.addEventListener('click',function(ev){
+         if(ev.target.id === 'grey'){
+          body.style.backgroundColor = ev.target.id
+         }
+
+         if(ev.target.id === 'white'){
+          body.style.backgroundColor = ev.target.id
+         }
+
+         if(ev.target.id === 'blue'){
+          body.style.backgroundColor = ev.target.id
+         }
+         if(ev.target.id === 'yellow'){
+          body.style.backgroundColor = ev.target.id
+         }
+     });  
+
+
+});
+

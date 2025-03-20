3bff0c63a85991fd7f78b735d9bf6606fae502c9
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-20 07:53:30 +0000

    pro

--- 09_advance/promise23.js
+++ 09_advance/promise23.js
@@ -84,3 +84,16 @@ async function consumepromisefive(){
 }
 
 consumepromisefive()
+
+async function getallures(){
+  try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+     const data= await response.json()
+     console.log(data);
+  }
+  catch(error){
+       console.log("ERROR:")
+  }
+    
+}
+getallures()

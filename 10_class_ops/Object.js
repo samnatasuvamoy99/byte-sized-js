21f1db2bf48af1d3a419fb368328d43f3b45a710
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-21 10:29:41 +0000

    prototype

---
+++ 10_class_ops/Object.js
@@ -0,0 +1,24 @@
+// prototype
+function multiple(num){
+   return num*5;
+}
+multiple.power =2;
+console.log( multiple(5));
+console.log(multiple.power);
+console.log(multiple.prototype);
+
+function createuser(username  , score){
+   this.username=username;
+   this.score=score;
+
+}
+createuser.prototype.increment= function(){
+  this.score++;
+}
+createuser.prototype.printMe = function(){
+  console.log(`score is ${this.score}`);
+}
+
+const con =createuser("suvamoy" , 8)
+const con1 =createuser("samanta" , 9)
+ con.printMe()

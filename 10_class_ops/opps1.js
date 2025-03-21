946664b11fd20a17fe45c365af5a734088810fe7
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-21 07:53:31 +0000

    object literal

---
+++ 10_class_ops/opps1.js
@@ -0,0 +1,17 @@
+const user = {
+  username:"suvamoy99",
+  password:"suvamoy775hfh",
+  Logincount:9,
+  SigneIn:true,
+
+  getuserDeatilas:function(){
+         //console.log("get user details from database"
+
+           console.log(this); // it will print your all curent context;
+
+         console.log(`Username:${this.username}`);
+  }
+
+}
+console.log(user.password);
+//console.log(user.getuserDeatilas());

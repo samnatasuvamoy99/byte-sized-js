6276488d3e10ac249b528ecea3a6ee11ee8a9860
suvamoy samanta <154976882+samnatasuvamoy99@users.noreply.github.com>
2025-03-25 10:01:27 +0000

    inheritances

--- 10_class_ops/notes.md
+++ 10_class_ops/notes.md
@@ -22,3 +22,10 @@ polymorphism
 
 # new
 this keyword is constracter function that make multiple instances . 
+
+## class
+prototype  key word will use to add any number of function  under  the main class 
+
+2.   if you have add to class then use extends key word
+
+3.super key word automaticlly know which class that you call any obj.name(username);
---
+++ 11_class/inheritance.js
@@ -0,0 +1,28 @@
+class user{
+   constructor(username){
+       this.username=username
+   }
+
+   logMe(){
+       console.log(`username is ${this.username}`)
+   }
+}
+
+// teacher class ko add kerna he user class se 
+
+class Teacher {
+     constructor(username,email,password){
+      super(username)
+      this.email=email
+      
+      this.password=password
+     }
+     addcourse(){
+          console.log(`A new course was add by ${this.username}`);
+     }
+}
+
+
+const teacher= new Teacher("suvamoy99" , "suvamoysamanta907@gmail.come" , "suvamoy7967@")
+
+teacher.addcourse();
